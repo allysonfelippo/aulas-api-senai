c22ea9dab319bd30ab5ad126b62f3295080065ea
ALLYSON FELIPPO BREDA DE MONTE CHRISTO <allyson_christo@estudante.sesisenai.org.br>
2024-05-21 22:13:17 -0300

    complemento

--- src/clientes.js
+++ src/clientes.js
@@ -1,18 +1,27 @@
 const express = require('express');
-const port = 3000;
+const port = 3010;
 
 const app = express();
 app.use(express.json());
 
+var clientes = [];
+
 app.get('/clientes', (req, res) => {
-    res.send('Chamou o GET!');
+ 
+    res.send(clientes);
 });
 
-app.post('/', (req, res) => {
-    res.send('Chamou o POST!');
+app.post('/clientes', (req, res) => {
+    const novoCliente = ( req.body)
+    clientes.push(novoCliente)
+    console.log(novoCliente);
+    res.send(novoCliente);
 });
 
 app.put('/', (req, res) => {
+
+
+
     res.send('Chamou o PUT!');
 });
 
